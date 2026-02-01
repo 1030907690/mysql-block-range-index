797394aca2b2a4a4b92aab8ddb0ec1e783c3b222
zhouzhongqing <zhouzhongqing@qq.com>
2026-02-01 21:44:09 +0800

    feat:first commit

---
+++ src/main/java/com/zzq/mysqlblockrangeindex/MysqlBlockRangeIndexApplication.java
@@ -0,0 +1,13 @@
+package com.zzq.mysqlblockrangeindex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysqlBlockRangeIndexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MysqlBlockRangeIndexApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zzq/mysqlblockrangeindex/MysqlBlockRangeIndexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zzq.mysqlblockrangeindex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MysqlBlockRangeIndexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
