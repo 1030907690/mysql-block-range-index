d9022d2e578ef919291add15955afe0750308b9a
zzq <1030907690@qq.com>
2026-02-02 18:54:17 +0800

    lib

--- mysql-block-range-index-sample/src/main/java/com/zzq/mysqlblockrangeindex/MysqlBlockRangeIndexSampleApplication.java
+++ mysql-block-range-index-sample/src/main/java/com/zzq/mysqlblockrangeindex/MysqlBlockRangeIndexSampleApplication.java
@@ -8,6 +8,18 @@ public class MysqlBlockRangeIndexSampleApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MysqlBlockRangeIndexSampleApplication.class, args);
+		  String sql = "SELECT id FROM  eb_user WHERE account = ? and real_name = ? or district = ? or a = ?";
+        Select select = (Select) CCJSqlParserUtil.parse(sql);
+
+        PlainSelect plainSelect = (PlainSelect) select.getSelectBody();
+        Expression where = plainSelect.getWhere();
+        AndExpression and = new AndExpression(where, CCJSqlParserUtil.parseCondExpression("b = ? and a = ?"));
+        System.out.println(where);
+        plainSelect.setWhere(and);
+        System.out.println(plainSelect.getWhere());
+
+
+
 	}
 
 }
