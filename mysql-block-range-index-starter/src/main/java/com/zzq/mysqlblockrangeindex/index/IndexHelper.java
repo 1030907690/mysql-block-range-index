8fe8e5de8b375b2a994194b150803d46a7de3eb0
zhouzhongqing <zhouzhongqing@qq.com>
2026-03-29 21:17:22 +0800

    refactor:根据时间范围获取id范围

--- mysql-block-range-index-starter/src/main/java/com/zzq/mysqlblockrangeindex/index/IndexHelper.java
+++ mysql-block-range-index-starter/src/main/java/com/zzq/mysqlblockrangeindex/index/IndexHelper.java
@@ -1,6 +1,8 @@
 package com.zzq.mysqlblockrangeindex.index;
 
 import com.baomidou.mybatisplus.annotation.TableName;
+import com.zzq.mysqlblockrangeindex.bean.Range;
+import com.zzq.mysqlblockrangeindex.parser.SelectParser;
 import org.springframework.util.Assert;
 import org.springframework.util.ObjectUtils;
 
@@ -13,7 +15,15 @@ import java.time.LocalDateTime;
  */
 
 public class IndexHelper {
+    private final static SelectParser selectParser = new SelectParser();
 
+    /**
+     * 开启时间范围索引
+     * @param tableName
+     * @param tableAlias
+     * @param startTime
+     * @param endTime
+     */
     public static void startDateRangeIndex(String tableName, String tableAlias, LocalDateTime startTime, LocalDateTime endTime) {
         if (ObjectUtils.isEmpty(startTime) && ObjectUtils.isEmpty(endTime)) {
             return;
@@ -36,5 +46,20 @@ public class IndexHelper {
         startDateRangeIndex(entityClz, null, startTime, endTime);
     }
 
+    /**
+     * 根据时间范围获取id范围
+     * @param tableName
+     * @param startTime
+     * @param endTime
+     * @return
+     */
+    public static Range getRangeByDateTime(String tableName, LocalDateTime startTime, LocalDateTime endTime) {
+        return selectParser.deduceRange(tableName, startTime, endTime);
+    }
 
+    public static Range getRangeByDateTime(Class<?> entityClz, LocalDateTime startTime, LocalDateTime endTime) {
+        TableName tableName = entityClz.getAnnotation(TableName.class);
+        Assert.notNull(tableName, "entityClz must be annotated with @TableName");
+        return getRangeByDateTime(tableName.value(), startTime, endTime);
+    }
 }
